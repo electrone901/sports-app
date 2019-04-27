52e999e4120569fe8e340579690654882f098d49
hamzaowais <hamza.agreed@gmail.com>
2019-04-27 16:23:11 -0400

    <CSS>

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button,Container,Row,Col} from 'react-bootstrap';
 
 const home = () => {
     return (
-        <div className="container">
+		<div className="container">
+			<Container>
+  				<Row>
+    				
+  				</Row>
+			</Container>
             <h1 className="home__header">Home Page</h1>
             <Button variant="secondary">Secondary</Button>
+            <Button variant="primary">Primary</Button>
         </div>
     );
 };
