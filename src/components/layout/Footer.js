ba1ce9a7db9883bbb52aaee5af65b91ac465b355
Song <yousonghou@yahoo.com>
2019-04-28 23:44:28 +0000

    fix bugs

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -20,9 +20,7 @@ export default() => {
 
             <section className="footer">
                 <div className="container">
-                    
                         Sweat Meet Mockup : https://xd.adobe.com/view/2ab3de5f-dbb0-41ac-645b-6f1a5182435d-5c82/
-                    
                 </div>
 	        </section>
             
